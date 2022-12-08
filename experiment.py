fba3ef42e9b1016e09f19f9e9fbcd27ee0d6d801
Aditya Bhave <aabhave@uci.edu>
2022-12-08 04:18:50 -0800

    Minor

--- experiment.py
+++ experiment.py
@@ -5,8 +5,10 @@ plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 columns = ["N", "H_Lower_Bound" ,"Min_Path_Cost","RunTime"]
 df = pd.read_csv("BNBvSLS_bnb.csv", names=columns)
-plt.plot(df.N,df.)
+plt.plot(df.N,df.H_Lower_Bound,label = 'Heuristic Lower Bound')
+plt.plot(df.N,df.Min_Path_Cost, label = 'Minimum Cost Path')
 plt.xlabel('Number of Nodes')
 plt.ylabel('Runtime in Seconds')
-plt.title('RuntTime analysis of SLS algorithm')
+plt.title('Heuristic Analysis')
+plt.legend()
 plt.show()
